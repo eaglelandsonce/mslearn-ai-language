cb6a31710efb7608466e44fd915c1af6d402aecb
eaglelandsonce <66654533+eaglelandsonce@users.noreply.github.com>
2026-02-10 20:47:28 -0500

    Refactor speaking-clock.py for improved functionality
    Refactor speaking-clock.py to improve structure and functionality. Update command processing and speech synthesis methods.

--- Labfiles/07-speech/Python/speaking-clock/speaking-clock.py
+++ Labfiles/07-speech/Python/speaking-clock/speaking-clock.py
@@ -1,23 +1,49 @@
 import os
-from datetime import datetime
+import datetime
 from dotenv import load_dotenv
 
 # Import namespaces
+from azure.core.credentials import AzureKeyCredential
 import azure.cognitiveservices.speech as speech_sdk
-from playsound import playsound
 
 
-def transcribe_command(speech_config):
+def main():
+    try:
+        global speech_config
+
+        # Get config settings
+        load_dotenv()
+        speech_key = os.getenv('SPEECH_KEY')
+        speech_region = os.getenv('SPEECH_REGION')
+
+        # Configure speech service
+        speech_config = speech_sdk.SpeechConfig(speech_key, speech_region)
+        print('Ready to use speech service in:', speech_config.region)
+
+        # Get spoken command
+        command = TranscribeCommand()
+
+        # Process command
+        if command.lower() == "what time is it?":
+            TellTime()
+        else:
+            print('You said:', command)
+
+    except Exception as ex:
+        print(ex)
+
+
+def TranscribeCommand():
     command = ''
 
     # Configure speech recognition
     current_dir = os.getcwd()
-    audioFile = current_dir + '\\time.wav'
-    playsound(audioFile)
+    audioFile = current_dir + '/time.wav'
     audio_config = speech_sdk.AudioConfig(filename=audioFile)
     speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
 
     # Process speech input
+    print("Listening...")
     speech = speech_recognizer.recognize_once_async().get()
     if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
         command = speech.text
@@ -29,56 +55,28 @@ def transcribe_command(speech_config):
             print(cancellation.reason)
             print(cancellation.error_details)
 
-    # Return the command
     return command
 
 
-def tell_time(speech_config):
-    # Get the current time
-    now = datetime.now()
+def TellTime():
+    now = datetime.datetime.now()
     response_text = 'The time is {}:{:02d}'.format(now.hour, now.minute)
 
     # Configure speech synthesis
-    speech_config.speech_synthesis_voice_name = 'en-GB-LibbyNeural'  # change this
-    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
+    output_file = "output.wav"
+    speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
+    audio_config = speech_sdk.audio.AudioConfig(filename=output_file)
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config, audio_config,)
 
     # Synthesize spoken output
-    responseSsml = " \
-        <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'> \
-            <voice name='en-GB-LibbyNeural'> \
-                {} \
-                <break strength='weak'/> \
-                Time to end this lab! \
-            </voice> \
-        </speak>".format(response_text)
-    speak = speech_synthesizer.speak_ssml_async(responseSsml).get()
+    speak = speech_synthesizer.speak_text_async(response_text).get()
     if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
         print(speak.reason)
+    else:
+        print("Spoken output saved in " + output_file)
 
     # Print the response
     print(response_text)
-
-
-def main():
-    try:
-        # Get Configuration Settings
-        load_dotenv()
-        cog_key = os.getenv('COG_SERVICE_KEY')
-        cog_region = os.getenv('COG_SERVICE_REGION')
-
-        # Configure speech service
-        speech_config = speech_sdk.SpeechConfig(cog_key, cog_region)
-        print('Ready to use speech service in:', speech_config.region)
-
-        # Get spoken command
-        command = transcribe_command(speech_config)
-
-        # Respond to command
-        if 'time' in command.lower():
-            tell_time(speech_config)
-
-    except Exception as ex:
-        print(ex)
 
 
 if __name__ == "__main__":
